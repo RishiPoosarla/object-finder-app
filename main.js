aec2908862d278e10d93e55770cd0c3317baa021
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-08-02 19:20:41 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+s = "";
+function setup(){
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+}
+function draw(){
+    image(video, 0, 0, 480, 380);
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelloaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    object = document.getElementById("object_name").value;
+}
+function modelloaded(){
+    console.log("Model loaded!");
+    s = true;
+}
