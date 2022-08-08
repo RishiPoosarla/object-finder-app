fe1b6dc0b481d7df5dd934da618a731d5847bd0a
RishiPoosarla <88695245+RishiPoosarla@users.noreply.github.com>
2022-08-07 19:59:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 s = "";
+objects = [];
 function setup(){
     canvas = createCanvas(480, 380);
     canvas.center();
@@ -7,6 +8,31 @@ function setup(){
 }
 function draw(){
     image(video, 0, 0, 480, 380);
+    if(s != ""){
+        objectDetector.detect(video, gotResults);
+        for(i=0 ; i < objects.length; i++){
+        if(objects[i].label == object){
+            video.stop();
+            objectDetector.detect(gotResults);
+            document.getElementById("status").innerHTML = "Object Mentioned Found";
+            synth = window.speechSynthesis;
+            utterthis = new SpeechSynthesisUtterance(object + "found");
+            synth.speak(utterthis);
+            console.log(object);
+        }
+        else{
+            document.getElementById("status").innerHTML = object + "Not Found";
+        }
+            document.getElementById("status").innerHTML = "Staus: Objects Detected";
+            document.getElementById("numberOfObjects").innerHTML = "Number of Objects Detected Are: " + objects.length; 
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 function start(){
     objectDetector = ml5.objectDetector('cocossd', modelloaded);
@@ -16,4 +42,13 @@ function start(){
 function modelloaded(){
     console.log("Model loaded!");
     s = true;
+}
+function gotResults(error, results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
